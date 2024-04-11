2dd1656a8de4d4c4ddd46947546d2804ce6c395e
~~SANTHOSH <119999002+SANTHOSHKUMARKANTE@users.noreply.github.com>
2024-04-11 22:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //complete this code
 class Person {
 	constructor(name, age){
-		this.name =  name;
-		this.age = age;
+		this._name =  name;
+		this._age = age;
 	}
 
 	
 	set age(age){
-		this.age = 10;
+		this._age = 10;
 	}
 	get name(){
-		console.log(this.name)
+		console.log(this._name)
 	}
 }
 
